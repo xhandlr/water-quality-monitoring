9e710043f8b08b8c1a5eb66e48984de4f8ce8e39
xhandlr <c.elgueta04@ufromail.cl>
2025-11-05 23:39:50 -0300

    chore: agregar react-dom para manejar rutas entre componentes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Navbar />
+      <div className="pt-20"> {/* push content below fixed navbar */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   </StrictMode>,
 )
